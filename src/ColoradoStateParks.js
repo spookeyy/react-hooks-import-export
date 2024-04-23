14b3f178874b1573dfc375de17a97cc0d8d51431
spookeyy <pangasmeshack@gmail.com>
2024-04-23 21:15:53 +0300

    slight changes

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,14 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import * as RMFunctions from "./parks/RockyMountain"
 
-function ColoradoStateParks() {
+export default function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
-
+  console.log(RMFunctions.trees); //named export 'trees'
+  console.log(RMFunctions.wildlife); //another named export
+  // output => "Elk, Bighorn Sheep, Moose"
+  
   return <h1>Colorado State Parks!</h1>;
 }
+
+
